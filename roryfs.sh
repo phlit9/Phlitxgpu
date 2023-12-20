1444cbeabc27a53224184c82412f382a994ea1a0
phlit9 <149636145+phlit9@users.noreply.github.com>
2023-12-19 18:23:25 -0600

    Update roryfs.sh

--- roryfs.sh
+++ roryfs.sh
@@ -36,33 +36,3 @@ echo "STEP 8 of 10: Replaced ETH address in config with 0x18C1c90101aA2D04B62a8F
 
 sudo pip install -U -r requirements.txt > /dev/null 2>&1
 echo "STEP 9 of 10: Installed Python Requirements"
-
-echo "STEP 10 of 10: Starting Miner & 10 GPUs"
-sudo nohup python3 miner.py --gpu=true > miner.log 2>&1 &
-sleep 1
-sudo nohup ./xengpuminer -d0 > xengpuminer-0.log 2>&1 &
-sleep 1
-sudo nohup ./xengpuminer -d1 > xengpuminer-1.log 2>&1 &
-sleep 1
-sudo nohup ./xengpuminer -d2 > xengpuminer-2.log 2>&1 &
-sleep 1
-sudo nohup ./xengpuminer -d3 > xengpuminer-3.log 2>&1 &
-sleep 1
-sudo nohup ./xengpuminer -d4 > xengpuminer-4.log 2>&1 &
-sleep 1
-sudo nohup ./xengpuminer -d5 > xengpuminer-5.log 2>&1 &
-sleep 1
-sudo nohup ./xengpuminer -d6 > xengpuminer-6.log 2>&1 &
-sleep 1
-sudo nohup ./xengpuminer -d7 > xengpuminer-7.log 2>&1 &
-sleep 1
-sudo nohup ./xengpuminer -d8 > xengpuminer-8.log 2>&1 &
-sleep 1
-sudo nohup ./xengpuminer -d9 > xengpuminer-9.log 2>&1 &
-sleep 1
-
-echo "-------------------------"
-echo "    MINING  XENBLOCKS    "
-echo "-------------------------"
-echo " "
-tail -f /root/XENGPUMiner/miner.log
